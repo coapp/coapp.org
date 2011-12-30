d98476430c8795436111a4cb833debaddabd3daa
Garrett Serack <garretts@microsoft.com>
2011-12-30 00:24:20 -0800

    added simple package feed

---
+++ src/static/scripts/feed.js
@@ -0,0 +1,41 @@
+
+jQuery(function() {
+
+    jQuery.getPackageFeed({
+        url: '/packages.xml',
+        success: function(feed) {
+            jQuery('#result').append('<h2>'
+            + '<a href="'
+            + feed.link
+            + '">'
+            + feed.title
+            + '</a>'
+            + '</h2>');
+            
+            var html = '';
+            
+            for(var i = 0; i < feed.items.length && i < 5; i++) {
+            
+                var item = feed.items[i];
+                
+                html += '<h3>'
+                + '<a href="'
+                + item.link
+                + '">'
+                + item.title
+                + '</a>'
+                + '</h3>';
+                
+                html += '<div class="updated">'
+                + item.updated
+                + '</div>';
+                
+                html += '<div>'
+                + item.description
+                + '</div>';
+            }
+            
+            jQuery('#result').append(html);
+        }    
+    });
+});
---
+++ src/static/scripts/jquery.jfeed.js
@@ -0,0 +1,251 @@
+/* Simple JavaScript Inheritance
+ * By John Resig http://ejohn.org/blog/simple-javascript-inheritance/
+ * MIT Licensed.
+ */
+// Inspired by base2 and Prototype
+(function($) {
+    (function() {
+        var initializing = false, fnTest = /xyz/ .test(function() { xyz; }) ? /\b_super\b/ : /.*/ ;
+        // The base Class implementation (does nothing)
+        this.Class = function() {
+        };
+
+        // Create a new Class that inherits from this class
+        Class.extend = function(prop) {
+            var _super = this.prototype;
+
+            // Instantiate a base class (but only create the instance,
+            // don't run the init constructor)
+            initializing = true;
+            var prototype = new this();
+            initializing = false;
+
+            // Copy the properties over onto the new prototype
+            for (var name in prop) {
+                // Check if we're overwriting an existing function
+                prototype[name] = typeof prop[name] == "function" &&
+                    typeof _super[name] == "function" && fnTest.test(prop[name]) ?
+                    (function(name, fn) {
+                        return function() {
+                            var tmp = this._super;
+
+                            // Add a new ._super() method that is the same method
+                            // but on the super-class
+                            this._super = _super[name];
+
+                            // The method only need to be bound temporarily, so we
+                            // remove it when we're done executing
+                            var ret = fn.apply(this, arguments);
+                            this._super = tmp;
+
+                            return ret;
+                        };
+                    })(name, prop[name]) :
+                    prop[name];
+            }
+
+            // The dummy class constructor
+
+            function Class() {
+                // All construction is actually done in the init method
+                if (!initializing && this.init)
+                    this.init.apply(this, arguments);
+            }
+
+            // Populate our constructed prototype object
+            Class.prototype = prototype;
+
+            // Enforce the constructor to be what we expect
+            Class.prototype.constructor = Class;
+
+            // And make this class extendable
+            Class.extend = arguments.callee;
+
+            return Class;
+        };
+    })();
+
+
+    var Person = Class.extend({
+        name: '',
+        uri: '',
+        email: '',
+        init: function(entry) {
+            if (entry != null) {
+                this.name = entry.find('name').eq(0).text();
+                this.uri = entry.find('uri').eq(0).text();
+                this.email = entry.find('email').eq(0).text();
+            }
+        }
+    });
+
+    var Item = Class.extend({
+        id: '',
+        title: '',
+        summary: '',
+        updated: '',
+        published: '',
+        author: { },
+        link: '',
+        links: [],
+        description: '',
+        categories: [],
+        pkg: { },
+        init: function(entry) {
+            this.id = entry.find('id').eq(0).text();
+            this.title = entry.find('title').eq(0).text();
+            var sum = entry.find('summary').eq(0);
+            this.summary = sum.attr('type') == 'html' ? sum.text() : '<pre>' + sum.text() + '</pre>';
+            this.updated = entry.find('updated').eq(0).text();
+            this.published = entry.find('published').eq(0).text();
+            this.author = new Person(entry.find('author').eq(0));
+
+            var links = this.links;
+            entry.find('link').each(function() {
+                var link = jQuery(this);
+                if (link.attr('rel') == 'enclosure') {
+                    links.push(new Link(jQuery(this)));
+                }
+            });
+
+            this.link = entry.find('link').eq(0).attr('href');
+            this.description = entry.find('content').eq(0).text();
+
+            var categories = this.categories;
+            entry.find('category').each(function() {
+                categories.push(new Category(jQuery(this)));
+            });
+        }
+    });
+
+    var Link = Class.extend({
+        url: '',
+        title: '',
+
+        init: function(entry) {
+            if (entry != null) {
+                url = entry.attr('href');
+                title = entry.attr('title');
+            }
+        }
+    });
+
+    var Category = Class.extend({
+        term: '',
+        label: '',
+        scheme: '',
+
+        init: function(entry) {
+            if (entry != null) {
+                term = entry.attr('term');
+                label = entry.attr('label');
+                scheme = entry.attr('scheme');
+            }
+        }
+    });
+
+    var Package = Class.extend({
+        name: '',
+        architecture: '',
+        version: '',
+        publicKeyToken: '',
+        displayName: '',
+        publisherDirectory: '',
+
+        bindingPolicyMinVersion: 0,
+        bindingPolicyMaxVersion: 0,
+
+        dependencies: [],
+        features: [],
+        requiredFeatures: [],
+        packageFeeds: [],
+        authorVersion: '',
+        bugTracker: '',
+        icon: '',
+        licenses: [],
+        nsfw: false,
+        stability: 0,
+
+        init: function(entry) {
+        }
+    });
+
+    var AtomFeed = Class.extend({
+        type: 'atom',
+        version: '',
+        title: '',
+        link: '',
+        description: '',
+
+        init: function(xml) {
+            if (jQuery.browser.msie) {
+                var xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+                xmlDoc.loadXML(xml);
+                xml = xmlDoc;
+            }
+
+            var channel = jQuery('feed', xml).eq(0);
+
+            this.version = '1.0';
+            this.title = jQuery(channel).find('title:first').text();
+            this.link = jQuery(channel).find('link:first').attr('href');
+            this.description = jQuery(channel).find('subtitle:first').text();
+            this.language = jQuery(channel).attr('xml:lang');
+            this.updated = jQuery(channel).find('updated:first').text();
+
+            this.items = new Array();
+
+            var feed = this;
+
+            jQuery('entry', xml).each(function() {
+                feed.items.push(new Item(jQuery(this)));
+            });
+        }
+    });
+
+
+    $.fn.getPackageFeed = function(options) {
+
+        options = $.extend({
+            url: null,
+            data: null,
+            cache: true,
+            success: null,
+            failure: null,
+            error: null,
+            global: true
+        }, options);
+
+        if (options.url) {
+            if (jQuery.isFunction(options.failure) && jQuery.type(options.error) === 'null') {
+                // Handle legacy failure option
+                options.error = function(xhr, msg, e) {
+                    options.failure(msg, e);
+                };
+            } else if (jQuery.type(options.failure) === jQuery.type(options.error) === 'null') {
+                // Default error behavior if failure & error both unspecified
+                options.error = function(xhr, msg, e) {
+                    window.console && console.log('getPackageFeed failed to load feed', xhr, msg, e);
+                };
+            }
+
+            return $.ajax({
+                type: 'GET',
+                url: options.url,
+                data: options.data,
+                cache: options.cache,
+                dataType: (jQuery.browser.msie) ? "text" : "xml",
+                success: function(xml) {
+                    var feed = new AtomFeed(xml);
+                    if (jQuery.isFunction(options.success)) {
+                        options.success(feed);
+                    }
+                },
+                error: options.error,
+                global: options.global
+            });
+        }
+    };
+
+    $.getPackageFeed = $.fn.getPackageFeed;
+})(jQuery);
