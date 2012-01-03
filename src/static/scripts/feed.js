b50f534f78ecfceb9f312f8b8794a760ba64a38f
Garrett Serack <garretts@microsoft.com>
2012-01-03 12:02:51 -0800

    reversed order of feed

--- src/static/scripts/feed.js
+++ src/static/scripts/feed.js
@@ -9,8 +9,8 @@ jQuery(function () {
         success: function (feed) {
             var html = '';
 
-            for (var i = 0; i < feed.items.length && i < 10; i++) {
-                html += '<h3><a href="{link}">{title}</a></h3><div class="updated">{updated}</div><div>{description}</div>\n'.Format(feed.items[i]);
+            for (var i = feed.items.length; i >0 && i > feed.items.length-10; i++) {
+                html += '<h3><a href="{link}">{title}</a></h3><div class="updated">{updated}</div><div>{description}</div>\n'.Format(feed.items[i-1]);
             }
 
             jQuery('#result').append(html);
