1dec63a3ea9516950d324e1adfc2cde79429c89e
Garrett Serack <garretts@microsoft.com>
2012-03-06 13:55:20 -0800

    made package feed unlimited length

--- src/static/scripts/feed.js
+++ src/static/scripts/feed.js
@@ -9,7 +9,7 @@ jQuery(function () {
         success: function (feed) {
             var html = '';
 
-            for (var i = feed.items.length-1; i >= 0 && i > feed.items.length-10; i--) {
+            for (var i = feed.items.length-1; i >= 0 && i > 0; i--) {
                 html += '<h3><a href="{link}">{title}</a></h3><div class="updated">{updated}</div><div>{description}</div>\n'.Format(feed.items[i]);
             }
 
