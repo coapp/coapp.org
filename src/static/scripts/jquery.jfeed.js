f5e3197cba620b2be50a28c3200362a83f0c4607
Scotis <Scott@Scott-PC.(none)>
2012-06-01 19:32:55 -0700

    Updating package list for new XML format

--- src/static/scripts/jquery.jfeed.js
+++ src/static/scripts/jquery.jfeed.js
@@ -114,258 +114,6 @@
         return str === null || str == undefined;
     };
 
-    var Person = Class.extend({
-        name: '',
-        uri: '',
-        email: '',
-        init: function (entry) {
-            if (entry != null) {
-                this.name = entry.find('name').eq(0).text();
-                this.uri = entry.find('uri').eq(0).text();
-                this.email = entry.find('email').eq(0).text();
-            }
-        }
-    });
-
-    var Item = Class.extend({
-        id: '',
-        title: '',
-        summary: '',
-        updated: '',
-        published: '',
-        author: {},
-        link: '',
-        links: [],
-        description: '',
-        categories: [],
-        pkg: {},
-
-        formatTextAsHtml: function (text, textType) {
-            if ((textType == "text") || text.indexOf('<') == -1 || text.indexOf('>') == -1) {
-                return '<pre>{0}</pre>'.Format(text);
-            }
-            return text;
-        },
-
-        init: function (entry) {
-            this.id = entry.find('id').eq(0).text();
-            this.title = entry.find('title').eq(0).text();
-            var sum = entry.find('summary').eq(0);
-            // this.summary = sum.attr('type') == 'html' ? sum.text() : '<pre>' + sum.text() + '</pre>';
-
-            this.summary = this.formatTextAsHtml(sum.text(), sum.attr('type'));
-
-            this.updated = entry.find('updated').eq(0).text();
-            this.published = entry.find('published').eq(0).text();
-            this.author = new Person(entry.find('author').eq(0));
-
-            var links = new Array();
-            entry.find('link').each(function () {
-                var link = jQuery(this);
-                if (link.attr('rel') == 'enclosure') {
-                    links.push(new Link(jQuery(this)));
-                }
-            });
-            this.links = links;
-
-            this.link = entry.find('link').eq(0).attr('href');
-            var desc = entry.find('content').eq(0);
-            this.description = this.formatTextAsHtml(desc.text(), desc.attr('type'));
-
-            var categories = new Array();
-            entry.find('category').each(function () {
-                categories.push(new Category(jQuery(this)));
-            });
-            this.categories = categories;
-            
-            this.pkg = new Package(entry.find('[nodeName="coapp:Package"]').eq(0));
-        }
-    });
-
-    var Link = Class.extend({
-        url: '',
-        title: '',
-
-        init: function (entry) {
-            if (entry != null) {
-                this.url = entry.attr('href');
-                this.title = entry.attr('title');
-            }
-        }
-    });
-
-    var Category = Class.extend({
-        term: '',
-        label: '',
-        scheme: '',
-
-        init: function (entry) {
-            if (entry != null) {
-                this.term = entry.attr('term');
-                this.label = entry.attr('label');
-                this.scheme = entry.attr('scheme');
-            }
-        }
-    });
-
-    var Package = Class.extend({
-        vendor: '',
-        name: '',
-        architecture: '',
-        version: '',
-        publicKeyToken: '',
-        displayName: '',
-        publisherDirectory: '',
-        
-        flavor: '',
-        variation: '',
-        shortName: '',
-
-        bindingPolicyMinVersion: 0,
-        bindingPolicyMaxVersion: 0,
-
-        roles: [],
-        dependencies: [],
-        features: [],
-        requiredFeatures: [],
-        packageFeeds: [],
-        authorVersion: '',
-        bugTracker: '',
-        icon: '',
-        licenses: [],
-        nsfw: false,
-        stability: 0,
-
-        init: function (entry) {
-            if (entry != null) {
-                this.vendor = entry.attr('Vendor');
-                this.name = entry.attr('Name');
-                this.architecture = entry.attr('Architecture');
-                this.version = entry.attr('Version');
-                this.publicKeyToken = entry.attr('PublicKeyToken');
-                this.displayName = entry.attr('DisplayName');
-                
-                this.bindingPolicyMinVersion = entry.find('[nodeName="coapp:BindingPolicyMinVersion"]').eq(0).text();
-                this.bindingPolicyMaxVersion = entry.find('[nodeName="coapp:BindingPolicyMaxVersion"]').eq(0).text();
-                
-                var dependencies = new Array();
-                entry.find('[nodeName="coapp:Dependencies"]').eq(0).find('[nodeName="coapp:guid"]').each(function () {
-                    dependencies.push( jQuery(this).text() );
-                });
-                this.dependencies = dependencies;
-                
-                var roles = new Array();
-                entry.find('[nodeName="coapp:Roles"]').eq(0).find('[nodeName="coapp:Role"]').each(function () {
-                    roles.push(new Role(jQuery(this)));
-                });
-                this.roles = roles;
-                
-                var packageFeeds = new Array();
-                entry.find('[nodeName="coapp:PackageFeeds"]').eq(0).find('[nodeName="coapp:string"]').each(function () {
-                    packageFeeds.push( jQuery(this).text() );
-                });
-                this.packageFeeds = packageFeeds;
-                
-                var details = entry.find('[nodeName="coapp:Details"]').eq(0);
-                
-                this.authorVersion = details.find('[nodeName="coapp:AuthorVersion"]').eq(0).text();
-                this.bugTracker = details.find('[nodeName="coapp:BugTracker"]').eq(0).text();
-                this.nsfw = details.find('[nodeName="coapp:IsNsfw"]').eq(0).text();
-                this.stability = details.find('[nodeName="coapp:Stability"]').eq(0).text();
-                
-                var licenses = new Array();
-                details.find('[nodeName="coapp:Licenses"]').eq(0).find('[nodeName="coapp:License"]').each(function () {
-                    licenses.push(new License(jQuery(this)));
-                });
-                this.licenses = licenses;
-                
-                //this.IconLocations = details.find('[nodeName="coapp:IconLocations"]').eq(0).text();
-                
-                var name = this.name;
-                var variation = '';
-                var flavor = '';
-                
-                var first_dash = name.indexOf('-');
-                
-                var last_begin_bracket = name.lastIndexOf('[');
-                var last_end_bracket = name.lastIndexOf(']');
-                
-                if(first_dash != -1)
-                {
-                    if(last_begin_bracket != -1)
-                    {
-                        if(first_dash > last_end_bracket)
-                        {
-                            variation = name.slice(first_dash + 1);
-                            
-                            if( check_variation_validity(variation) )
-                            {
-                                name = name.slice(0, first_dash);
-                            }
-                        }
-                        else if(first_dash < last_begin_bracket)
-                        {
-                            variation = name.slice(first_dash + 1, last_begin_bracket);
-                            
-                            if( check_variation_validity(variation) )
-                            {
-                                name = name.slice(0, first_dash) + name.slice(last_begin_bracket);
-                                
-                                var last_begin_bracket = name.lastIndexOf('[');
-                                var last_end_bracket = name.lastIndexOf(']');
-                            }
-                        }
-                    }
-                    else
-                    {
-                        variation = name.substr(first_dash + 1);
-                        
-                        if( check_variation_validity(variation) )
-                        {
-                            name = name.slice(0, first_dash);
-                        }
-                    }
-                }
-                
-                if(last_begin_bracket != -1)
-                {
-                    flavor = name.slice(last_begin_bracket + 1, last_end_bracket);
-                    name = name.slice(0, last_begin_bracket);
-                }
-                
-                this.shortName = name;
-                this.variation = variation;
-                this.flavor = flavor;
-            }
-        }
-    });
-    
-    var Role = Class.extend({
-        name: '',
-        packageRole: '',
-
-        init: function (entry) {
-            if (entry != null) {
-                this.name = entry.find('[nodeName="coapp:Name"]').eq(0).text();
-                this.packageRole = entry.find('[nodeName="coapp:PackageRole"]').eq(0).text();
-            }
-        }
-    });
-    
-    var License = Class.extend({
-        licenseId: '',
-        name: '',
-        licenseUrl: '',
-
-        init: function (entry) {
-            if (entry != null) {
-                this.licenseId = entry.find('[nodeName="coapp:LicenseId"]').eq(0).text();
-                this.name = entry.find('[nodeName="coapp:Name"]').eq(0).text();
-                this.licenseUrl = entry.find('[nodeName="coapp:LicenseUrl"]').eq(0).text();
-            }
-        }
-    });
-
     var AtomFeed = Class.extend({
         type: 'atom',
         version: '',
@@ -396,6 +144,254 @@
             jQuery('entry', xml).each(function () {
                 feed.items.push(new Item(jQuery(this)));
             });
+        }
+    });
+
+    var Item = Class.extend({
+        id: '',
+        title: '',
+        summary: '',
+        updated: '',
+        published: '',
+        author: {},
+        link: '',
+        links: [],
+        description: '',
+        categories: [],
+        pkg: {},
+
+        formatTextAsHtml: function (text, textType) {
+            if ((textType == "text") || text.indexOf('<') == -1 || text.indexOf('>') == -1) {
+                return '<pre>{0}</pre>'.Format(text);
+            }
+            return text;
+        },
+
+        init: function (entry) {
+            var id = entry.find('id').eq(0).text();
+            id = id.replace(/(:|\.|\||;|\[|\])/g,'-');
+            this.id = id;
+            this.title = entry.find('title').eq(0).text();
+            var sum = entry.find('summary').eq(0);
+            // this.summary = sum.attr('type') == 'html' ? sum.text() : '<pre>' + sum.text() + '</pre>';
+
+            this.summary = this.formatTextAsHtml(sum.text(), sum.attr('type'));
+
+            this.updated = entry.find('updated').eq(0).text();
+            this.published = entry.find('published').eq(0).text();
+            this.author = new Person(entry.find('author').eq(0));
+
+            var links = new Array();
+
+            entry.find('link').each(function () {
+                var link = jQuery(this);
+                if (link.attr('rel') == 'enclosure') {
+                    links.push(new Link(jQuery(this)));
+                }
+            });
+
+            this.links = links;
+
+            this.link = entry.find('link').eq(0).attr('href');
+
+            var desc = entry.find('content').eq(0);
+            this.description = this.formatTextAsHtml(desc.text(), desc.attr('type'));
+
+            var categories = new Array();
+
+            entry.find('category').each(function () {
+                categories.push(new Category(jQuery(this)));
+            });
+
+            this.categories = categories;
+            
+            this.pkg = new Package(entry.find('[nodeName="coapp:Package"]').eq(0));
+        }
+    });
+
+    var Person = Class.extend({
+        name: '',
+        uri: '',
+        email: '',
+        init: function (entry) {
+            if (entry != null) {
+                this.name = entry.find('name').eq(0).text();
+                this.uri = entry.find('uri').eq(0).text();
+                this.email = entry.find('email').eq(0).text();
+            }
+        }
+    });
+
+    var Link = Class.extend({
+        url: '',
+        title: '',
+        rel: '',
+        type: '',
+
+        init: function (entry) {
+            if (entry != null) {
+                this.url = entry.attr('href');
+                this.title = entry.attr('title');
+                this.rel = entry.attr('rel');
+                this.type = entry.attr('type');
+            }
+        }
+    });
+
+    var Category = Class.extend({
+        term: '',
+        label: '',
+        scheme: '',
+
+        init: function (entry) {
+            if (entry != null) {
+                this.term = entry.attr('term');
+                this.label = entry.attr('label');
+                this.scheme = entry.attr('scheme');
+            }
+        }
+    });
+
+    var Package = Class.extend({
+        vendor: '',
+        canonicalName: '',
+        name: '',
+        packageType: '',
+        flavor: '',
+        architecture: '',
+        version: '',
+        publicKeyToken: '',
+        displayName: '',
+        publisherDirectory: '',
+        
+        variation: '',
+        shortName: '',
+
+        bindingPolicyMinVersion: 0,
+        bindingPolicyMaxVersion: 0,
+
+        roles: [],
+        dependencies: [],
+        features: [],
+        requiredFeatures: [],
+        feeds: [],
+        authorVersion: '',
+        bugTracker: '',
+        icon: '',
+        licenses: [],
+        nsfw: false,
+        stability: 0,
+
+        init: function (entry) {
+            if (entry != null) {
+                this.vendor = entry.attr('Vendor');
+                this.canonicalName = entry.attr('CanonicalName');
+                this.name = entry.attr('Name');
+                this.packageType = entry.attr('PackageType');
+                this.flavor = entry.attr('Flavor');
+                this.architecture = entry.attr('Architecture');
+                this.version = entry.attr('Version');
+                this.publicKeyToken = entry.attr('PublicKeyToken');
+                this.displayName = entry.attr('DisplayName');
+                
+                var bindingPolicy = entry.find('[nodeName="coapp:BindingPolicy"]').eq(0);
+
+                this.bindingPolicyMinVersion = bindingPolicy.attr('Minimum');
+                this.bindingPolicyMaxVersion = bindingPolicy.attr('Maximum');
+
+                //this.bindingPolicyMaxVersion = entry.find('[nodeName="coapp:BindingPolicyMaxVersion"]').eq(0).text();
+                
+                var dependencies = new Array();
+
+                entry.find('[nodeName="coapp:Dependencies"]').eq(0).find('item').each(function () {
+                    var dep_id = jQuery(this).attr('key');
+                    dep_id = dep_id.replace(/(:|\.|\||;|\[|\])/g,'-');
+                    dependencies.push( dep_id );
+
+                    /*jQuery(this).find('[nodeName="coapp:List"]').eq(0).find('[nodeName="coapp:Uri"]').each(function () {
+                        var dep_list = jQuery(this).text();
+                    });*/
+                });
+
+                this.dependencies = dependencies;
+                
+                var roles = new Array();
+
+                entry.find('[nodeName="coapp:Roles"]').eq(0).find('[nodeName="coapp:Role"]').each(function () {
+                    roles.push(new Role(jQuery(this)));
+                });
+
+                this.roles = roles;
+                
+                var feeds = new Array();
+
+                entry.find('[nodeName="coapp:Feeds"]').eq(0).find('[nodeName="coapp:Uri"]').each(function () {
+                    feeds.push( jQuery(this).text() );
+                });
+
+                this.feeds = feeds;
+                
+                var details = entry.find('[nodeName="coapp:Details"]').eq(0);
+                
+                this.authorVersion = details.find('[nodeName="coapp:AuthorVersion"]').eq(0).text();
+                this.bugTracker = details.find('[nodeName="coapp:BugTracker"]').eq(0).text();
+                this.nsfw = details.find('[nodeName="coapp:IsNsfw"]').eq(0).text();
+                this.stability = details.find('[nodeName="coapp:Stability"]').eq(0).text();
+                
+                var licenses = new Array();
+
+                details.find('[nodeName="coapp:Licenses"]').eq(0).find('[nodeName="coapp:License"]').each(function () {
+                    licenses.push(new License(jQuery(this)));
+                });
+
+                this.licenses = licenses;
+                
+                //this.IconLocations = details.find('[nodeName="coapp:IconLocations"]').eq(0).text();
+                
+                var name = this.name;
+                var variation = '';
+                
+                var first_dash = name.indexOf('-');
+                
+                if(first_dash != -1)
+                {
+                    variation = name.substr(first_dash + 1);
+                    
+                    if( check_variation_validity(variation) )
+                    {
+                        name = name.slice(0, first_dash);
+                    }
+                }
+                
+                this.shortName = name;
+                this.variation = variation;
+            }
+        }
+    });
+    
+    var Role = Class.extend({
+        name: '',
+        packageRole: '',
+
+        init: function (entry) {
+            if (entry != null) {
+                this.name = entry.attr('Name');
+                this.packageRole = entry.attr('PackageRole');
+            }
+        }
+    });
+    
+    var License = Class.extend({
+        licenseId: '',
+        name: '',
+        licenseUrl: '',
+
+        init: function (entry) {
+            if (entry != null) {
+                this.licenseId = entry.find('[nodeName="coapp:LicenseId"]').eq(0).text();
+                this.name = entry.find('[nodeName="coapp:Name"]').eq(0).text();
+                this.licenseUrl = entry.find('[nodeName="coapp:LicenseUrl"]').eq(0).text();
+            }
         }
     });
 
